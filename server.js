e3ac3be6c184874a297412d672e89ed988b42d5e
Digidigidoom <tomshahar91@gmail.com>
2025-04-22 15:21:04 +0300

    Add routing for services

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ import express from 'express'
 const app = express() 
 app.get('/', (req, res) => res.send('Hello there')) 
 app.listen(3030, () => console.log('Server ready at port 3030'))
+
+// //* Express Config:
+// app.use(express.static('public'))
+
+
+app.get('/api/bug', (req, res) => {res.send('bug')}) 
+app.get('/api/bug/save', (req, res) => {res.send('bug/save')}) 
+app.get('/api/bug/:bugId', (req, res) => {res.send('bug/bogid')}) 
+app.get('/api/bug/:bugId/remove', (req, res) => {res.send('bug/bogid/remove')}) 
+
+
+
